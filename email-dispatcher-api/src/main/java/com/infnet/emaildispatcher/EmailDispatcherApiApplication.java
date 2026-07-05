3c63cb49830a306a3e84eca4b2cbfb0346d39058
guiSoaresCavalcante <gui.cavalcante1999@outlook.com.br>
2026-07-05 00:46:57 -0300

    feat: add inital project

---
+++ email-dispatcher-api/src/main/java/com/infnet/emaildispatcher/EmailDispatcherApiApplication.java
@@ -0,0 +1,13 @@
+package com.infnet.emaildispatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailDispatcherApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailDispatcherApiApplication.class, args);
+	}
+
+}
---
+++ email-dispatcher-api/src/test/java/com/infnet/emaildispatcher/EmailDispatcherApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infnet.emaildispatcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailDispatcherApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
