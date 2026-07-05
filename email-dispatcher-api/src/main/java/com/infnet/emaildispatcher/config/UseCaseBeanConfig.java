6ac8c3e2f7759c45adb9b7c381165c14b62973e9
guiSoaresCavalcante <gui.cavalcante1999@outlook.com.br>
2026-07-05 19:37:27 -0300

    refactor: move crm provider to a variable

--- email-dispatcher-api/src/main/java/com/infnet/emaildispatcher/config/UseCaseBeanConfig.java
+++ email-dispatcher-api/src/main/java/com/infnet/emaildispatcher/config/UseCaseBeanConfig.java
@@ -8,15 +8,19 @@ import com.infnet.emaildispatcher.application.port.out.email.IEmailGenerationPub
 import com.infnet.emaildispatcher.application.port.out.email.IEmailSender;
 import com.infnet.emaildispatcher.application.usecase.CrmUseCase;
 import com.infnet.emaildispatcher.application.usecase.EmailUseCase;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class UseCaseBeanConfig {
 
+    @Value("${crm.provider}")
+    private String crmProvider;
+
     @Bean
     public ICrmUseCase crmUseCase(ICrmProviderFactory crmProviderFactory) {
-        return new CrmUseCase(crmProviderFactory, CrmProviderType.PLOOMES);
+        return new CrmUseCase(crmProviderFactory, CrmProviderType.valueOf(crmProvider));
     }
 
     @Bean
